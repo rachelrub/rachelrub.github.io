6a646089bd172465a195e59626da0647b8772295
rachelrub <rachel.rub14@gmail.com>
2016-06-11 15:25:30 +0200

    Editing my portfolio website

---
+++ day7morning.js
@@ -0,0 +1,19 @@
+// window.onload = function() {
+	// console.log the values of First Name and Last Name 
+	function getFormValues() { 
+		var first = document.getElementById("fname").value;
+		var last = document.getElementById("lname").value;
+		console.log(first + " " + last);
+
+	}
+
+	// Change the color of the div with id "color-div"
+	function changeColor() { 
+  		var change = document.getElementById("color-div");
+  		change.style.color = "red";
+
+
+	}
+
+// }
+
